9a110575e1d210dd9fd053ea83cbd01e2750b559
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-06-30 13:03:55 -0300

    feat: add routing with react-router-dom.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Register from './pages/Register'
+import ReactDOM from "react-dom/client";
+import Login from './pages/Login'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-
-      <Register/>
-
-    </div>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element= {<Login/>}/>
+      </Routes>
+      </BrowserRouter>
+     
   )
 }
 
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Form from '../components/register/Form'
-import Questions from '../components/register/Questions'
-
+ 
 function register() {
   return (
     <div className="w-full max-w-md">
         <Form/>
-        <Questions/>
-    </div>
+     </div>
   )
 }
 
