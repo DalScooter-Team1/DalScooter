48e3c452ceb5e79d26aa7ba282ce2a2878284293
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-07-12 13:16:36 -0300

    fix: correct route order for Login and Register pages in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,10 +9,11 @@ function App() {
   return (
      <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/register' element={<Register />} />
+
+        <Route path='/' element={<Register />} />
         <Route path='/login' element= {<Login/>}/>
         <Route path='/admin-dashboard' element={<AdminDashboard/>}/>
+        
       </Routes>
       </BrowserRouter>
      
