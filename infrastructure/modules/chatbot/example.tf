97401cc3d2a58ca314d2c5bc59644266b31014cf
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2025-07-25 11:16:50 -0300

    feat: expand sample utterances for flower ordering intent in chatbot module

--- infrastructure/modules/chatbot/example.tf
+++ infrastructure/modules/chatbot/example.tf
@@ -4,20 +4,50 @@ resource "aws_lexv2models_intent" "example" {
   name        = "example_intent"
   locale_id   = aws_lexv2models_bot_locale.dalscooter_chatbot_locale.locale_id
   description = "An example intent for the DalScooter chatbot"
+
   sample_utterance {
     utterance = "I want to order flowers"
   }
+  sample_utterance {
+    utterance = "Order flowers for me"
+  }
+  sample_utterance {
+    utterance = "Can you get me some flowers?"
+  }
+  sample_utterance {
+    utterance = "Send me some flowers"
+  }
+  sample_utterance {
+    utterance = "I need flowers"
+  }
+  sample_utterance {
+    utterance = "I’d like to get flowers"
+  }
+  sample_utterance {
+    utterance = "Flowers please"
+  }
+  sample_utterance {
+    utterance = "I want flowers"
+  }
+  sample_utterance {
+    utterance = "I need to order flowers"
+  }
+  sample_utterance {
+    utterance = "Can I order flowers?"
+  }
+  sample_utterance {
+    utterance = "I need some flowers"
+  }
 
-    initial_response_setting {
-      initial_response {
-        message_group {
-            message {
-              plain_text_message {
-                value = "Your order for flowers has been placed."
-              }
-            }
+  initial_response_setting {
+    initial_response {
+      message_group {
+        message {
+          plain_text_message {
+            value = "Your order for flowers has been placed."
           }
         }
       }
-
+    }
+  }
 }
